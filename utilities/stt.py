d9a4f359440c0515b60d4f1189888b96fb96457e
a0533 <7198754@gmail.com>
2025-09-08 17:43:13 +0300

    הוספת מחלקת WhisperService לתמלול אודיו באופן אסינכרוני, שילוב לוגים, ושיפור מבנה הקוד.

--- utilities/stt.py
+++ utilities/stt.py
@@ -1,24 +1,42 @@
 import whisper
+import logging
+import asyncio
 
-model = whisper.load_model(
-    name="tiny",
-    download_root=r"C:\models\whisper"
-)
+logger = logging.getLogger(__name__)
 
-# load audio and pad/trim it to fit 30 seconds
-audio = whisper.load_audio(r"C:\podcasts\download (1).wav")
-audio = whisper.pad_or_trim(audio)
 
-# make log-Mel spectrogram and move to the same device as the model
-mel = whisper.log_mel_spectrogram(audio, n_mels=model.dims.n_mels).to(model.device)
+class WhisperService:
+    def __init__(
+            self,
+            model_name: str = "tiny",
+            download_root: str = r"C:\models\whisper"
+    ):
+        self.model = whisper.load_model(
+            name=model_name,
+            download_root=download_root
+        )
+    async def whisper_transcribe(self, file_path, file_hash: str):
+        file_hash = file_hash
+        audio = whisper.load_audio(file_path)
+        audio = whisper.pad_or_trim(audio)
+        mel = whisper.log_mel_spectrogram(audio, n_mels=self.model.dims.n_mels).to(self.model.device)
+        _, probs = self.model.detect_language(mel)
+        logger.info(f"Detected language: {max(probs, key=probs.get)}")
+        options = whisper.DecodingOptions()
+        result = whisper.decode(self.model, mel, options)
+        logger.debug(f"Transcription: {result.text}")
 
-# detect the spoken language
-_, probs = model.detect_language(mel)
-print(f"Detected language: {max(probs, key=probs.get)}")
+        return result.text, file_hash
 
-# decode the audio
-options = whisper.DecodingOptions()
-result = whisper.decode(model, mel, options)
 
-# print the recognized text
-print(result.text)
+if __name__ == "__main__":
+    whisper_service = WhisperService()
+
+    text, file_hash = asyncio.run(
+            whisper_service.whisper_transcribe(
+            r"C:\podcasts\download (6).wav",
+            "jhgyuftydtuyytftrdtr"
+        )
+    )
+    print(text)
+    print(file_hash)
