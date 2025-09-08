20be4e6ced9d9f0f2577993517e3b31b012a539b
a0533 <7198754@gmail.com>
2025-09-08 17:46:07 +0300

    שיפור אתחול מחלקת WhisperService על ידי העברת פרמטרים ברורים והסרת קוד מיותר.

--- utilities/stt.py
+++ utilities/stt.py
@@ -8,15 +8,14 @@ logger = logging.getLogger(__name__)
 class WhisperService:
     def __init__(
             self,
-            model_name: str = "tiny",
-            download_root: str = r"C:\models\whisper"
+            model_name: str,
+            download_root: str
     ):
         self.model = whisper.load_model(
             name=model_name,
             download_root=download_root
         )
     async def whisper_transcribe(self, file_path, file_hash: str):
-        file_hash = file_hash
         audio = whisper.load_audio(file_path)
         audio = whisper.pad_or_trim(audio)
         mel = whisper.log_mel_spectrogram(audio, n_mels=self.model.dims.n_mels).to(self.model.device)
@@ -30,7 +29,10 @@ class WhisperService:
 
 
 if __name__ == "__main__":
-    whisper_service = WhisperService()
+    whisper_service = WhisperService(
+        model_name="tiny",
+        download_root=r"C:\models\whisper"
+    )
 
     text, file_hash = asyncio.run(
             whisper_service.whisper_transcribe(
