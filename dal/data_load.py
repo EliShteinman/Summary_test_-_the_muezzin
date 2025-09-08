79b9a89bce49ac3c83123ce2d49fca4f3f6dfc19
a0533 <7198754@gmail.com>
2025-09-08 12:56:35 +0300

    הוספת לוגים, החזרת מידע מפורט מה-API, ושיפורים במבנה הקוד לקראת הוספת הלוג שקיבלנו

--- dal/data_load.py
+++ dal/data_load.py
@@ -1,14 +1,16 @@
+import logging
+import os
 from datetime import datetime
 from pathlib import Path
-import os
 from pprint import pprint
-import logging
+
 import config
 
 logger = logging.getLogger(__name__)
 
 
 def load_meta_data_for_directory(directory_path):
+    logger.debug(f"Loading metadata from directory {directory_path}")
     if not os.path.exists(directory_path):
         logger.warning(f"Directory {directory_path} does not exist.")
         raise FileNotFoundError(f"Directory {directory_path} does not exist.")
@@ -16,8 +18,10 @@ def load_meta_data_for_directory(directory_path):
     for file in directory_path.iterdir():
         yield load_meta_data_for_file(file)
 
-def load_meta_data_for_file(fath):
-    file = Path(fath)
+
+def load_meta_data_for_file(file_path):
+    logger.debug(f"Loading metadata from file {file_path}")
+    file = Path(file_path)
     if not file.exists():
         logger.warning(f"File {file} does not exist.")
         return None
@@ -29,22 +33,27 @@ def load_meta_data_for_file(fath):
                 "file_suffix": file.suffix.replace(".", ""),
                 "file_name": file.stem,
                 "file_size": file.stat().st_size,
-                "file_creation_time": datetime.fromtimestamp(file.stat().st_ctime).strftime("%Y-%m-%d %H:%M:%S"),
-                "file_modification_time": datetime.fromtimestamp(file.stat().st_mtime).strftime("%Y-%m-%d %H:%M:%S"),
-                "file_access_time": datetime.fromtimestamp(file.stat().st_atime).strftime("%Y-%m-%d %H:%M:%S"),
-            }
+                "file_creation_time": datetime.fromtimestamp(
+                    file.stat().st_ctime
+                ).strftime("%Y-%m-%d %H:%M:%S"),
+                "file_modification_time": datetime.fromtimestamp(
+                    file.stat().st_mtime
+                ).strftime("%Y-%m-%d %H:%M:%S"),
+                "file_access_time": datetime.fromtimestamp(
+                    file.stat().st_atime
+                ).strftime("%Y-%m-%d %H:%M:%S"),
+            },
         }
-        logger.info(f"Metadata loaded from {file} : {meta_data}")
+        logger.debug(f"Metadata loaded from {file} : {meta_data}")
         return meta_data
     else:
         logger.warning(f"File {file} does not have a .wav extension.")
         return None
 
 
-
 if __name__ == "__main__":
-    # for data in load_meta_data_for_directory(config.DIRECTORY_PATH):
-    #     pprint(data)
+    for data in load_meta_data_for_directory(config.DIRECTORY_PATH):
+        pprint(data)
 
     print(Path(r"C:\podcasts\download.wav"))
     meta_data = load_meta_data_for_file(Path(r"C:\podcasts\download.wav"))
--- dal/main.py
+++ dal/main.py
@@ -1,29 +1,31 @@
 import asyncio
 import logging
-import uvicorn
-from pathlib import Path
-from data_load import load_meta_data_for_directory, load_meta_data_for_file
 from contextlib import asynccontextmanager
+from pathlib import Path
+
+import uvicorn
+from numpy.ma.core import count
+
+from data_load import load_meta_data_for_directory, load_meta_data_for_file
 from fastapi import FastAPI, HTTPException, status
+
 import config
 from utilities.kafka.async_client import KafkaProducerAsync
+
 logging.basicConfig(level=config.LOG_LEVEL)
 logging.getLogger("kafka").setLevel(level=config.LOG_KAFKA)
 logger = logging.getLogger(__name__)
 
 producer: KafkaProducerAsync | None = None
 
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     global producer
     logger.info("Starting retriever service...")
     boostrap_servers = f"{config.KAFKA_URL}:{config.KAFKA_PORT}"
-    producer = KafkaProducerAsync(
-        bootstrap_servers=boostrap_servers
-    )
-    logger.info(
-        f"Initializing Kafka producer - {producer.get_config()}"
-    )
+    producer = KafkaProducerAsync(bootstrap_servers=boostrap_servers)
+    logger.info(f"Initializing Kafka producer - {producer.get_config()}")
 
     try:
         await producer.start()
@@ -32,7 +34,8 @@ async def lifespan(app: FastAPI):
         logger.error(f"Failed to start Kafka producer: {e}")
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f"Failed to start Kafka producer: {e}", )
+            detail=f"Failed to start Kafka producer: {e}",
+        )
 
     logger.info("Starting main processing loop...")
 
@@ -40,9 +43,10 @@ async def lifespan(app: FastAPI):
 
     logger.info("Application shutdown...")
     try:
-        pass
+        producer.stop()
     except Exception as e:
         logger.error(f"Error during shutdown: {e}")
+
 
 app = FastAPI(
     lifespan=lifespan,
@@ -54,6 +58,7 @@ app = FastAPI(
 
 @app.get("/")
 async def root():
+    logger.debug("health check is running")
     return {"message": "Podcast Retriever API"}
 
 
@@ -61,30 +66,43 @@ async def root():
 async def load_file(file_path: Path):
     logger.info(f"Loading file: {file_path}")
     try:
-        logger.info(Path(file_path))
+        logger.debug(Path(file_path))
         meta_data = load_meta_data_for_file(file_path)
-        await producer.send_message(
-            config.KAFKA_OUTPUT_TOPIC, meta_data
-        )
-        return {"status": "success"}
+        logger.debug(meta_data)
+        await producer.send_message(config.KAFKA_OUTPUT_TOPIC, meta_data)
+        return {
+            "status": "success",
+            "file_path": meta_data["file_path"],
+            "meta_data": meta_data["meta_data"],
+        }
     except Exception as e:
         logger.error(f"Error in main loop: {e}")
-        return {"status": "error"}
-
+        return {
+            "status": "error",
+            "error_message": f"Error loading file: {e}",
+        }
 
 
 @app.get("/load_directory/{directory_path}")
 async def load_directory(directory_path: Path):
     logger.info(f"Loading directory: {directory_path}")
     try:
+        num_files = 0
+        results = []
         for meta_data in load_meta_data_for_directory(directory_path):
-            await producer.send_message(
-                config.KAFKA_OUTPUT_TOPIC, meta_data
-            )
-        return {"status": "success"}
+            logger.debug(meta_data)
+            num_files += 1
+            results.append(meta_data)
+            await producer.send_message(config.KAFKA_OUTPUT_TOPIC, meta_data)
+        return {
+            "status": "success",
+            "num_files": num_files,
+            "results": results,
+        }
     except Exception as e:
         logger.error(f"Error in main loop: {e}")
         return {"status": "error"}
+
 
 if __name__ == "__main__":
     uvicorn.run(app=app, host="0.0.0.0", port=8000)
